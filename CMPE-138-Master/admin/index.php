fb53cc52ef81fb0034ce92c112bca7c11a52fa53
ElizabethNa7 <63435879+ElizabethNa7@users.noreply.github.com>
2020-11-23 03:06:41 -0800

    Update index.php

--- CMPE-138-Master/admin/index.php
+++ CMPE-138-Master/admin/index.php
@@ -9,31 +9,6 @@ if (!isset($_SESSION['loggedin'])) {
 include("../db.php");
 
 error_reporting(0);
-if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='sign_out')
-{
-$mem_id=$_GET['mem_id'];
-
-/*this is update query*/
-mysqli_query($con,"update member set mem_status=0 where mem_id='$mem_id'")or die("update query is incorrect...");
-}
-if(isset($_GET['action']) && $_GET['action']!="" && $_GET['action']=='sign_in')
-{
-$mem_id=$_GET['mem_id'];
-
-/*this is update query*/
-mysqli_query($con,"update member set mem_status=1 where mem_id='$mem_id'")or die("update query is incorrect...");
-}
-
-$page=$_GET['page'];
-
-if($page=="" || $page=="1")
-{
-$page1=0;
-}
-else
-{
-$page1=($page*10)-10;
-}
 
 include "sidenav.php";
 include "topheader.php";
@@ -45,76 +20,83 @@ include "topheader.php";
          <div class="col-md-14">
            <div class="card ">
              <div class="card-header card-header-primary">
-               <h4 class="card-title">Member Status </h4>
+               <h4 class="card-title">Search Employees </h4>
              </div>
              <div class="card-body">
                <div class="table-responsive ps">
                  <table class="table table-hover tablesorter " id="page1">
-                   <thead class=" text-primary">
-                     <tr><th>Member ID</th><th>Member Name</th><th>Member Level</th>
-                   </tr></thead>
+                   <div class="row">
+
+                   </div>
+
                    <tbody>
-                     <?php
-                       $result=mysqli_query($con,"select mem_id, mem_fname, mem_lname, mem_lvl from member where mem_status=1")or die ("query 1 incorrect.....");
 
-                       while(list($mem_id,$mem_fname,$mem_lname,$mem_lvl)=mysqli_fetch_array($result))
-                       {
-                       echo "<tr><td>$mem_id</td><td>$mem_fname $mem_lname</td><td>$mem_lvl</td>
 
-                       <td>
-                       <a class=' btn btn-danger' href='members.php?mem_id=$mem_id&action=sign_out'>Sign Out</a>
-                       </td></tr>";
-                       }
-                       $result=mysqli_query($con,"select mem_id, mem_fname, mem_lname, mem_lvl from member where mem_status=0")or die ("query 1 incorrect.....");
-
-                       while(list($mem_id,$mem_fname,$mem_lname,$mem_lvl)=mysqli_fetch_array($result))
-                       {
-                       echo "<tr><td>$mem_id</td><td>$mem_fname $mem_lname</td><td>$mem_lvl</td>
-
-                       <td>
-                       <a class=' btn btn-success' href='members.php?mem_id=$mem_id&action=sign_in'>Sign In</a>
-                       </td></tr>";
-                       }
-
-                       ?>
                    </tbody>
                  </table>
+                 <!DOCTYPE html>
+                 <html>
+                 <body>
+                 <object align= left>
+                 <form method="post">
+                 <label>Search</label>
+                 <input type="text" name="search">
+                 <input type="submit" name="submit">
+                 </object>
+                 </form>
+                 </body>
+                 </html>
+                 <br> <br>
+                 <?php
+
+                 $con = new PDO("mysql:host=localhost;dbname=gym_manage",'root','');
+                 $found;
+                 if (isset($_POST["submit"])) {
+                 	$str = $_POST["search"];
+                 	$sth = $con->prepare("SELECT e_id,emp_username,emp_fname,emp_lname,emp_phone,emp_position FROM `employee` WHERE e_id = '$str' OR emp_username = '$str' OR emp_fname = '$str' OR emp_lname = '$str' OR emp_phone = '$str' OR emp_position = '$str'");
+
+                 	$sth->setFetchMode(PDO:: FETCH_OBJ);
+                 	$sth -> execute();
+                 	while($row = $sth->fetch())
+                 	{
+                     $found = '1';
+                 		?>
+                     <div style="Color:black">
+                 		<table>
+                 			<tr>
+                 				<th>Employee ID &nbsp;|&nbsp; </th>
+                 				<th>Username &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp; </th>
+                         <th>First name &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;</th>
+                         <th>Last name &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp; </th>
+                         <th>Phone number &nbsp;|&nbsp; </th>
+                         <th>Position</th>
+                 			</tr>
+                 			<tr>
+                 				<td><?php echo $row->e_id; ?></td>
+                 				<td><?php echo $row->emp_username;?></td>
+                         <td><?php echo $row->emp_fname;?></td>
+                         <td><?php echo $row->emp_lname;?></td>
+                         <td><?php echo $row->emp_phone;?></td>
+                         <td><?php echo $row->emp_position;?></td>
+                 			</tr>
+
+                 		</table>
+                   </div>
+                 <?php
+                 	}
+                   if($found == '0'){
+                       echo '<i style="color:black; font-size:18px; font-style: normal;">
+                       Employee does not exist. </i> ';
+                     }
+
+                 }
+
+                 ?>
 
                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
              </div>
            </div>
-           <nav aria-label="Page navigation example">
-             <ul class="pagination">
-               <li class="page-item">
-                 <a class="page-link" href="#" aria-label="Previous">
-                   <span aria-hidden="true">&laquo;</span>
-                   <span class="sr-only">Previous</span>
-                 </a>
-               </li>
-                <?php
-//counting paging
 
-               $paging=mysqli_query($con,"select mem_id, mem_fname, mem_lname, mem_lvl from member");
-               $count=mysqli_num_rows($paging);
-
-               $a=$count/10;
-               $a=ceil($a);
-
-               for($b=1; $b<=$a;$b++)
-               {
-               ?>
-               <li class="page-item"><a class="page-link" href="members.php?page=<?php echo $b;?>"><?php echo $b." ";?></a></li>
-               <?php
-}
-?>
-               <li class="page-item">
-                 <a class="page-link" href="#" aria-label="Next">
-                   <span aria-hidden="true">&raquo;</span>
-                   <span class="sr-only">Next</span>
-                 </a>
-               </li>
-             </ul>
-           </nav>
          </div>
 
        </div>
