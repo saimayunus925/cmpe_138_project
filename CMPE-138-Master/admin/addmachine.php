7a971b6219e0fd10130b20e3208040a35ba3460a
aman-kaur11 <63080661+aman-kaur11@users.noreply.github.com>
2020-11-23 17:33:26 -0800

    Commented: Aman
    Added SJSU comment

--- CMPE-138-Master/admin/addmachine.php
+++ CMPE-138-Master/admin/addmachine.php
@@ -2,7 +2,7 @@
 <?php
 // We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page
 if (!isset($_SESSION['loggedin'])) {
   header('Location: ../emp-login.php');
   exit;
